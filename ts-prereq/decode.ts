28e4a4162cd23cf0595556730e9b49c4be6deac3
Berkay ÇIRAK <81809211+berkaycirak@users.noreply.github.com>
2024-08-03 02:06:31 +0300

    Update decode.ts

--- ts-prereq/decode.ts
+++ ts-prereq/decode.ts
@@ -1,7 +1,7 @@
 import bs58 from 'bs58';
 
 const kp = bs58.decode(
-	'3rEZm5huaoumJ9gZhSUojCScbgB8tWp79rswDPeg5QBtnajVkBe2BZzjCcG3cr6jstWU8UVPnBmtdGVtmw8sbq8Y'
+	'privateKey'
 );
 
 console.log('My Secret key is:', kp);
